b93d4e93d9ba66087c536a17c2d72ea6efd27661
Naquiao <nacho-solorzano@hotmail.com>
2025-05-19 19:05:08 -0700

    Update setup.py to dynamically read version from bugster/__init__.py, ensuring accurate versioning for the package.

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 from setuptools import setup, find_packages
+import re
+
+# Read version from bugster/__init__.py
+with open("bugster/__init__.py", "r") as f:
+    version_match = re.search(r'__version__ = ["\']([^"\']*)["\']', f.read())
+    if version_match:
+        version = version_match.group(1)
+    else:
+        raise RuntimeError("Unable to find version string.")
 
 setup(
     name="bugster",
-    version="0.1.0",
+    version=version,
     packages=find_packages(),
     install_requires=[
         "typer[all]==0.15.4",

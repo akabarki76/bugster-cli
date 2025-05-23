3377358945104a0c9a8c811ea04e33ab00fe1624
Naquiao <nacho-solorzano@hotmail.com>
2025-05-23 13:37:16 -0700

    Remove cache module and update references to use BUGSTER_DIR constant for improved code organization and clarity.

--- bugster/analyzer/cache/__init__.py
+++
@@ -1,9 +0,0 @@
-from .main import (
-    DOT_BUGSTER_DIR_NAME,
-    DOT_BUGSTER_DIR_PATH,
-)
-
-__all__ = [
-    "DOT_BUGSTER_DIR_NAME",
-    "DOT_BUGSTER_DIR_PATH",
-]
--- bugster/analyzer/cache/main.py
+++
@@ -1,4 +0,0 @@
-import os
-
-DOT_BUGSTER_DIR_NAME = ".bugster"
-DOT_BUGSTER_DIR_PATH = os.path.join(os.getcwd(), DOT_BUGSTER_DIR_NAME)

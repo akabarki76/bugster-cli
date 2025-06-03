7cbb5f6ab433b138d2ab866e18e4bead7b92c1fd
Franco Sebastian Benitez <francosbenitez@gmail.com>
2025-06-03 10:27:10 -0300

    feat: ignore non javascript files

--- bugster/libs/utils/enums.py
+++ bugster/libs/utils/enums.py
@@ -9,9 +9,28 @@ class BugsterApiPath(str, Enum):
 
 
 class GitCommand(list, Enum):
-    """Git commands."""
+    """Git commands.
 
-    DIFF_STATUS = ["git", "status", "--porcelain"]
-    DIFF_FILES = ["git", "diff", "--name-only"]
-    DIFF_CHANGES = ["git", "diff", "--", "."]
-    DIFF_CACHED = ["git", "diff", "--cached"]
+    - DIFF_STATUS: Get the status of the files in the repository.
+    - DIFF_CHANGES: Get the changes in the files that have changed in the repository.
+    - DIFF_CACHED: Get the changes in the files that have been staged in the repository.
+    - DIFF_FILES: Get the files that have changed in the repository.
+    """
+
+    DIFF_STATUS = [
+        "git",
+        "status",
+        "--porcelain",
+        "--",
+        "*.tsx",
+        "*.ts",
+        "*.js",
+        "*.jsx",
+        "*.tsx",
+        "*.ts",
+        "*.js",
+        "*.jsx",
+    ]
+    DIFF_CHANGES = ["git", "diff", "--", "*.tsx", "*.ts", "*.js", "*.jsx"]
+    DIFF_CACHED = ["git", "diff", "--cached", "--", "*.tsx", "*.ts", "*.js", "*.jsx"]
+    DIFF_FILES = ["git", "diff", "--name-only"]  # Unused

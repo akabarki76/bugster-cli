87c7714dad35f29e9e10f8f7443a4d8b044b95ad
Franco Sebastian Benitez <francosbenitez@gmail.com>
2025-06-30 15:42:16 -0300

    feat: init

--- bugster/libs/services/test_cases_service.py
+++ bugster/libs/services/test_cases_service.py
@@ -14,7 +14,7 @@ from bugster.clients.http_client import BugsterHTTPClient
 from bugster.constants import BUGSTER_DIR, TESTS_DIR
 from bugster.libs.utils.enums import BugsterApiPath
 from bugster.libs.utils.errors import BugsterError
-from bugster.libs.utils.files import get_specs_paths
+from bugster.libs.utils.files import get_specs_pages, get_specs_paths
 from bugster.libs.utils.nextjs.extract_page_folder import extract_page_folder
 from bugster.utils.user_config import get_api_key
 
@@ -47,19 +47,27 @@ class TestCasesService:
 
     def __init__(self):
         """Initialize the service."""
-        self.analysis_json_path = None
+        self._analysis_json_path = None
 
-    def _set_analysis_json_path(self) -> str:
-        """Set the `analysis.json` file path."""
-        project_info = get_project_info()
-        cache_framework_dir = os.path.join(
-            BUGSTER_DIR, project_info["data"]["frameworks"][0]["id"]
-        )
-        self.analysis_json_path = os.path.join(cache_framework_dir, "analysis.json")
+    @property
+    def analysis_json_path(self) -> str:
+        """Get the `analysis.json` file path."""
+        if self._analysis_json_path is None:
+            project_info = get_project_info()
+            cache_framework_dir = os.path.join(
+                BUGSTER_DIR, project_info["data"]["frameworks"][0]["id"]
+            )
+            self._analysis_json_path = os.path.join(
+                cache_framework_dir, "analysis.json"
+            )
+        return self._analysis_json_path
 
-    def _init_generation(self, page_filter: Optional[List[str]] = None, count: Optional[int] = None) -> list[dict[Any, str]]:
+    def _init_generation(
+        self, page_filter: Optional[List[str]] = None, count: Optional[int] = None
+    ) -> list[dict[Any, str]]:
         """Post the `analysis.json` file to the API and receive the test cases."""
         logger.info("Posting analysis.json file to the API...")
+
         if not self.analysis_json_path:
             raise BugsterError("Analysis JSON path is not set")
 
@@ -67,18 +75,33 @@ class TestCasesService:
             raise BugsterError(
                 "Analysis JSON file not found, execute bugster analyze first"
             )
+
         data = {}
+
         if page_filter:
             data["page_filter"] = ",".join(page_filter)
+
+        def generate_context(page_filter: Optional[list[str]] = None):
+            """Generate context with existing specs."""
+            if page_filter:
+                pass
+
+            specs_pages = get_specs_pages()
+            pass
+
         if count is not None:
             data["count"] = count
-        with open(self.analysis_json_path, "r") as file:
+
+        with open(self.analysis_json_path) as file:
             analysis_data = yaml.safe_load(file)
             payload = {"json": analysis_data, "data": data}
+
             with BugsterHTTPClient() as client:
                 api_key = get_api_key()
+
                 if api_key:
                     client.set_headers({"x-api-key": api_key})
+
                 return client.post(
                     endpoint=BugsterApiPath.GENERATE_INIT.value,
                     json=payload,
@@ -139,8 +162,10 @@ class TestCasesService:
         """Get the status of a job."""
         with BugsterHTTPClient() as client:
             api_key = get_api_key()
+
             if api_key:
                 client.set_headers({"x-api-key": api_key})
+
             return client.get(
                 endpoint=BugsterApiPath.GENERATE_CHECK_RESULTS.value,
                 params={"job_id": job_id},
@@ -192,9 +217,10 @@ class TestCasesService:
             console.print("❌ Test generation timeout")
             return None
 
-    def generate_test_cases(self, page_filter: list[str] = None, count: Optional[int] = None):
+    def generate_test_cases(
+        self, page_filter: list[str] = None, count: Optional[int] = None
+    ):
         """Generate test cases for the given codebase analysis."""
-        self._set_analysis_json_path()
         result = self._init_generation(page_filter=page_filter, count=count)
         test_cases = self._polling_test_cases(result=result)
 
